4f05592ac6c1fb845a3b5f8b27335fefff38990b
Liam Wright <mail@liam-wright.co.uk>
2018-03-20 15:48:16 +0000

    Create ColourGuess.js
    TODO: Update formatting in proper editor

--- ColourGuess.js
+++ ColourGuess.js
@@ -1,4 +1,11 @@
-var colors = generateRandomColors(6);var h1 = document.querySelector("h1");var squares = document.querySelectorAll(".square");var pickedColor = pickColor();var messageDisplay = document.querySelector("#message");var colorDisplay = document.getElementById("colorDisplay");colorDisplay.textContent = pickedColor;for (var i = 0; i < squares.length; i++){ //Add colours to squares squares[i].style.backgroundColor = colors[i];
+var colors = generateRandomColors(6);
+var h1 = document.querySelector("h1");
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var messageDisplay = document.querySelector("#message");
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
+for (var i = 0; i < squares.length; i++){ //Add colours to squares squares[i].style.backgroundColor = colors[i];
  //Add listeners to squares squares[i].addEventListener("click", function() {  //Get color of clicked square  var clickedColor = this.style.backgroundColor;
   if (clickedColor === pickedColor)  {   messageDisplay.textContent = "Correct";   h1.style.background = clickedColor;   changeColors(clickedColor);  }  else  {   this.style.backgroundColor = "#232323";   messageDisplay.textContent = "Wrong";  } });
 }
